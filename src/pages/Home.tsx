ed17d630326db4969fe2fdb8e37fc9527573139d
Millenium-AI <milleniumautoai@gmail.com>
2025-12-28 18:50:53 -0500

    swap

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,8 +16,8 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
-      <Testimonials />
       <Contact />
+      <Testimonials />
       <Footer />
     </>
   );
