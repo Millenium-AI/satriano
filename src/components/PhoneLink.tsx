4cca63a5b447d1cf62deaa48a6f65948ac0f5533
Millenium-Ai <milleniumautoai@gmail.com>
2026-09-02 10:28:01 -0400

    again

--- src/components/PhoneLink.tsx
+++ src/components/PhoneLink.tsx
@@ -16,7 +16,7 @@ export default function PhoneLink({
   const telHref = `tel:${phone.replace(/[^\d+]/g, '')}`;
 
   return (
-    <a href={telHref} className={`marine-phone ${className || ''}`} style={style}>
+    <a id="marine-phone" href={telHref} className={className} style={style}>
       {children}
     </a>
   );
