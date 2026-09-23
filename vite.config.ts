26aa3dd31e6788b35c8e80551b548398d30bd1e4
Millenium-Ai <milleniumautoai@gmail.com>
2026-09-23 11:55:38 -0400

    update to optimitzation

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import galleryThumbsPlugin from './vite-plugin-gallery-thumbs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), galleryThumbsPlugin()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
